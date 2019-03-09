04b82fd3755aac604dfd2a51d0d0060a59906747
My Vo <my.n.vo26@gmail.com>
2019-03-09 07:32:22 -0800

    updated: Set up models and admin config

--- osd/store/models.py
+++ osd/store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
@@ -11,6 +12,9 @@ class Category(models.Model):
         ordering = ('name',)
         verbose_name ='category'
         verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('store:products_by_category', args=[self.slug])
 
     # to create  human readable representation
     def __str__(self):
