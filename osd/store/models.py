63d71660c04182be1571e15a775250f38689c9f4
Izzy Moriguchi <izumimoriguchi@gmail.com>
2019-03-16 00:07:07 -0700

    Fixed typo from shop to store

--- osd/store/models.py
+++ osd/store/models.py
@@ -40,7 +40,7 @@ class Product(models.Model):
         verbose_name_plural = 'products'
 
     def get_url(self):
-        return reverse('shop:ProdCatDetail', args=[self.category.slug, self.slug])
+        return reverse('store:ProdCatDetail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return '{}'.format(self.name)
