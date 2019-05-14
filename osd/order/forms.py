305179a51d99912bde9383792bfd39bc33e98b55
Austin Wang <acw154@gmail.com>
2019-05-13 19:29:41 -0700

    Validation for regex

--- osd/order/forms.py
+++ osd/order/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django import forms
 from django.core.validators import RegexValidator
+import re
+from django.core.exceptions import ValidationError
 
 
     # full_name = forms.CharField(max_length=100, required=True)
@@ -14,8 +16,15 @@ class addressForm(forms.Form):
     city = forms.CharField(max_length=100, required=True)
     state = forms.CharField(max_length=2, required=True, help_text= 'eg. CA')
     zip = forms.CharField(max_length=5, required=True)
-    numeric = RegexValidator(r'[0-9]+', 'Must be a 12 digit number')
-    credit_card_number = forms.CharField(label='Credit Card Number', max_length=12, min_length=12, required=True, validators=[numeric])
+    numeric = RegexValidator(r'^[0-9]+', 'Must be numeric')
+    # exp_date = RegexValidator(r'^[0-9]{2}/[0-9]{2}$')
+    credit_card_number = forms.CharField(label='Credit Card Number', max_length=12, min_length=12, required=True, validators=[num_test])
+    # cvv_number = forms.CharField(label='Credit Card CVV', max_length=3, min_length=3, required=True, validators=[numeric])
+    # expiration_date = forms.CharField(label='Card Expiration (MM/YYYY)', required=True, validators=[exp_date])
     # class Meta:
     #     model = Order
     #     fields = ('ship_address',)
+
+def num_test(val):
+    if not re.match(r'[0-9]+', val):
+        raise ValidationError('Must be numeric')
